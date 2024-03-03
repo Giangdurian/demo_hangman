0d590750718e1bac3fd68c651a7ae8b646c82987
GiangDurian <gianglessin2@gmail.com>
2024-03-03 21:44:04 +0700

    First commit

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string secretWord = "haha";
+    string guessedWord(secretWord.length(), '-');
+    cout << secretWord << endl;
+    cout << guessedWord << endl;
+}
